a20df72fac443e92dfcd90c38b5d19a448e6e4d0
anthonytranDev <anthonytran.dev@gmail.com>
2020-02-02 23:11:57 +0000

    feat: removed js core teaching materials

--- js-core/core-1/week-1/final/console.log/console.log-2.js
+++
@@ -1,7 +0,0 @@
-// Filename: console.log-2.js
-// Last Edited: 26 Sep 2019
-
-// Task) Add a newline above and below the string "Result: Hello World"
-console.log("")
-console.log("Result: Hello World!")
-console.log("")
--- js-core/core-1/week-1/final/functions/functions-4.js
+++
@@ -1,4 +0,0 @@
-// Filename: functions-3.js
-// Last Edited: 26 Sep 2019
-
-// Nested functions - no exercises for using nested functions
